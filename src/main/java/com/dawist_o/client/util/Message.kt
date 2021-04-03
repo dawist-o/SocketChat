7024150d9dcae65262af8270e79653cc9f29cbb2
Vladislav <vkoval2002@gmail.com>
2021-04-04 00:50:26 +0300

    File sending possibility

--- src/main/java/com/dawist_o/client/util/Message.kt
+++ src/main/java/com/dawist_o/client/util/Message.kt
@@ -1,6 +1,25 @@
 package com.dawist_o.client.util
 
+import java.io.File
 import java.io.Serializable
 
-data class Message(val client: Client, val msg:String): Serializable {
+class Message : Serializable {
+    var client: Client
+    var msg: String = ""
+    var file: File? = null
+
+    fun containsFile(): Boolean = file != null
+    override fun toString(): String {
+        return "Message(client=$client, msg='$msg', file=$file)"
+    }
+
+    constructor(client: Client, msg: String) {
+        this.client = client
+        this.msg = msg
+    }
+
+    constructor(client: Client, file: File) {
+        this.client = client
+        this.file = file
+    }
 }
