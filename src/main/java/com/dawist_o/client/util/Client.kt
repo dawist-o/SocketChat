796ca3aad4b70f281c47fce7fc60f9cc3c121976
Vladislav <vkoval2002@gmail.com>
2021-04-04 00:48:12 +0300

    Clear up

--- src/main/java/com/dawist_o/client/util/Client.kt
+++ src/main/java/com/dawist_o/client/util/Client.kt
@@ -1,19 +1,6 @@
 package com.dawist_o.client.util
+
 import java.io.Serializable
 
-import java.net.Socket
-
-data class Client(val name:String): Serializable{
-
-    /*override fun start(primaryStage: Stage?) {
-        StageController(primaryStage)
-    }
-
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            launch(Client::class.java)
-        }
-    }*/
-}
+data class Client(var name: String): Serializable
 
